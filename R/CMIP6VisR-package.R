a01df6a396e8401e6167a108c2b395dadb8ca540
Kevin Shook <kshook@kshook.ca>
2025-02-27 19:10:58 -0500

    Minor changes to DESCRIPTION as required by CRAN

--- R/CMIP6VisR-package.R
+++ R/CMIP6VisR-package.R
@@ -1,7 +1,7 @@
-#' @title TVisualization and Analysis of CMIP6 Hydroclimatic Data
+#' @title Visualization and Analysis of CMIP6 Hydroclimatic Data
 #' @name CMIP6VisR-package
 #' 
-#' @description  Provides data manipulation, analyses and visualization for CMIP6 hydroclimatic data. The files are been archived in the Federated Research Data Repository (FRDR; https://doi.org/10.20383/103.0829).
+#' @description  Provides data manipulation, analyses and visualization for Coupled Model Intercomparison Project, Phase-6 (CMIP6) hydroclimatic data. The files are archived in the Federated Research Data Repository (FRDR) (Rajulapati et al., 2024)
 
 #' @references Rajulapati, C., Abdelmoaty, H., Nerantzaki, S., Papalexiou, S. (2024). High-resolution future temperature and precipitation dataset for Canada, 2015 - 2100. Federated Research Data Repository. https://doi.org/10.20383/103.0829
 #' @keywords package
